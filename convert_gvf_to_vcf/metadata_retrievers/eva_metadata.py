6cabb48455d18010ad105f74e3b59f8737588a25
khetherin <hetherin@ebi.ac.uk>
2026-07-10 12:41:51 +0100

    refactor: change paths

--- convert_gvf_to_vcf/metadata_retrievers/eva_metadata.py
+++ convert_gvf_to_vcf/metadata_retrievers/eva_metadata.py
@@ -964,8 +964,10 @@ class EVAMetadataRetriever(BaseMetadataRetriever):
     # THESE GETTERS GET THE RELEVANT VALUE
     def _get_file_name(self, vcf_output):
         file_name = os.path.basename(vcf_output)
-        # study_name = file_name.split(".")[0]
-        # file_name = os.path.join(study_name, file_name)
+        study_name = file_name.split(".")[0]
+        # an export from bash
+        base_dir = os.environ.get("SUBMISSION_BASE_DIR", "submission")
+        file_name = os.path.join(base_dir, study_name, file_name)
         return file_name
 
     def _get_file_size(self, vcf_output):
